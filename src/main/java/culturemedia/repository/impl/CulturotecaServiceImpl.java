4c609885dca342dd82ea308553a5b46a15014c6a
Camilo Pérez <camiloperezpadilla7@gmail.com>
2024-10-11 11:21:16 -0500

    Adding Culturomedia structures

---
+++ src/main/java/culturemedia/repository/impl/CulturotecaServiceImpl.java
@@ -0,0 +1,24 @@
+package culturemedia.repository.impl;
+
+import culturemedia.model.Video;
+import culturemedia.model.View;
+import culturemedia.repository.CulturotecaService;
+
+import java.util.List;
+
+public class CulturotecaServiceImpl implements CulturotecaService {
+    @Override
+    public List<Video> findAll() {
+        return List.of();
+    }
+
+    @Override
+    public Video save(Video save) {
+        return null;
+    }
+
+    @Override
+    public void add(View view) {
+
+    }
+}
