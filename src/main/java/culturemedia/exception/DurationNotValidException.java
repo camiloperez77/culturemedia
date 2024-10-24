62982bf37d4c4dbe06ed620018283c42e2edb7a1
Camilo Pérez <camiloperezpadilla7@gmail.com>
2024-10-23 19:11:16 -0500

    Adding Video-Views Implementation

--- src/main/java/culturemedia/exception/DurationNotValidException.java
+++ src/main/java/culturemedia/exception/DurationNotValidException.java
@@ -1,7 +1,9 @@
 package culturemedia.exception;
 
+import java.text.MessageFormat;
+
 public class DurationNotValidException extends CulturotecaException {
-    public DurationNotValidException(String message) {
-        super(message);
+    public DurationNotValidException(String title, double duration) {
+        super(MessageFormat.format("Invalid duration for video: {}. Duration: {}", title, duration));
     }
 }
--- src/main/java/culturemedia/exception/VideoNotFoundException.java
+++ src/main/java/culturemedia/exception/VideoNotFoundException.java
@@ -1,7 +1,12 @@
 package culturemedia.exception;
 
+import java.text.MessageFormat;
+
 public class VideoNotFoundException extends CulturotecaException {
-    public VideoNotFoundException(String message) {
-        super(message);
+
+    public VideoNotFoundException() {super("Video not found");}
+
+    public VideoNotFoundException(String title) {
+        super(MessageFormat.format("Video not found: ", title));
     }
 }
