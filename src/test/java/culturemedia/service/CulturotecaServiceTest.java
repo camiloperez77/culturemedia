88c0c7ed8ac56fb6288efed62a8f6c7db68b61af
Camilo Pérez <camiloperezpadilla7@gmail.com>
2024-10-31 20:57:12 -0500

    Adding Mocking test on CulturotecaServiceTest

--- src/test/java/culturemedia/service/CulturotecaServiceTest.java
+++ src/test/java/culturemedia/service/CulturotecaServiceTest.java
@@ -3,76 +3,76 @@ package culturemedia.service;
 import culturemedia.exception.DurationNotValidException;
 import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
-import culturemedia.model.View;
 import culturemedia.repository.VideoRepository;
-import culturemedia.repository.ViewsRepository;
-import culturemedia.repository.impl.VideoRepositoryImpl;
-import culturemedia.repository.impl.ViewsRepositoryImpl;
+
 import culturemedia.service.impl.CulturotecaServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+
 class CulturotecaServiceTest {
 
 
-    private CulturotecaService culturotecaTest;
-    private CulturotecaService culturotecaTestVoid;
+    @Mock
+    private VideoRepository videoRepository;
+
+    @InjectMocks
+    private CulturotecaServiceImpl culturotecaService;
+
+    List<Video> videos = List.of(new Video("01", "Título 1", "----", 4.5),
+            new Video("02", "Título 2", "----", 5.5),
+            new Video("03", "Título 3", "----", 4.4),
+            new Video("04", "Título 4", "----", 3.5),
+            new Video("05", "Clic 5", "----", 5.7),
+            new Video("06", "Clic 6", "----", 5.1));
+
 
     @BeforeEach
-    void init() {
-        culturotecaTest = new CulturotecaServiceImpl(new VideoRepositoryImpl(), new ViewsRepositoryImpl());
-        culturotecaTestVoid = new CulturotecaServiceImpl(new VideoRepositoryImpl(), new ViewsRepositoryImpl());
-
-        List<Video> videos = List.of(new Video("01", "Título 1", "----", 4.5),
-                new Video("02", "Título 2", "----", 5.5),
-                new Video("03", "Título 3", "----", 4.4),
-                new Video("04", "Título 4", "----", 3.5),
-                new Video("05", "Clic 5", "----", 5.7),
-                new Video("06", "Clic 6", "----", 5.1));
-
-        for ( Video video : videos ) {
-			culturotecaTest.save( video );
-		}
-
-
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
     }
 
 
     @Test
     void when_FindAll_all_videos_should_be_returned_successfully() throws VideoNotFoundException{
-        List<Video> videos = culturotecaTest.findAll();
+        doReturn(videos).when(videoRepository).findAll();
         assertEquals(6, videos.size());
-
     }
 
     @Test
     void when_FindAll_does_not_find_any_video_an_VideoNotFoundException_should_be_thrown_successfully() throws VideoNotFoundException {
-        assertThrows(VideoNotFoundException.class, () -> {culturotecaTestVoid.findAll();});
+        assertThrows(VideoNotFoundException.class, () -> {culturotecaService.findAll();});
     }
 
      @Test
     void when_Find_a_video_and_should_be_returned_successfully() throws VideoNotFoundException {
-        List<Video> videos = culturotecaTest.find("Clic 6");
-        assertEquals(1, videos.size());
+         List<Video> videosEncontrados = List.of(new Video("06", "Clic 6", "----", 5.1));
+         doReturn(videosEncontrados).when(videoRepository).find("Clic 6");
+        assertEquals(1, videosEncontrados.size());
     }
 
         @Test
     void when_Find_does_not_find_any_video_with_title_an_VideoNotFoundException_should_be_thrown_successfully() throws VideoNotFoundException {
-        assertThrows(VideoNotFoundException.class, () -> {culturotecaTest.find("Proof 2");});
+        assertThrows(VideoNotFoundException.class, () -> {culturotecaService.find("Proof 2");});
     }
 
     @Test
     void when_Find_a_video_on_range_of_duration_should_be_returned_successfully() throws DurationNotValidException {
-        List<Video> videos = culturotecaTest.find(5.2, 5.8);
-        assertEquals(2, videos.size());
+        List<Video> videosEncontrados = List.of(new Video("02", "Título 2", "----", 5.5),
+                new Video("05", "Clic 5", "----", 5.7));
+        doReturn(videosEncontrados).when(videoRepository).find(5.2, 5.8);
+        assertEquals(2, videosEncontrados.size());
     }
 
     @Test
     void when_Find_does_not_find_any_video_with_range_of_duration_and_DurationNotFoundException_should_be_thrown_successfully() throws DurationNotValidException{
-        assertThrows(DurationNotValidException.class, () -> {culturotecaTest.find(7.0, 8.0);});
+        assertThrows(DurationNotValidException.class, () -> {culturotecaService.find(7.0, 8.0);});
     }
 }
